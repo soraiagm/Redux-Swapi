edcd7325a7e824850a07cbc5ea4908efd01cda79
Soraia <soraia@Soraias-iMac.local>
2019-07-21 15:06:32 -0400

    added dependencies to index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+// import { createStore } from "redux";
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware, createStore } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from "redux-logger";
 // rootReducer from ./reducers
+import rootReducer from "./reducers";
 
 const store = createStore(
-  rootReducer
   /* applyMiddleware goes here */
+  rootReducer, applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
